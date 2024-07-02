13aac97fce32bf5a96916839cfda4b9ce9003086
Sydney205 <diekechi@gmail.com>
2024-07-02 19:55:59 +0100

    webpack configured

---
+++ webpack.config.cjs
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: './server.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
