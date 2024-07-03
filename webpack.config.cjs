f7cc32f66a4d49323aa205ad8b26472f5d7b7034
Sydney205 <diekechi@gmail.com>
2024-07-03 19:55:01 +0100

    webpack-config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,11 +1,14 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './server.js',  // Adjust the entry point as per your project structure
+  entry: './server.js',  
+  target: 'node',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  externals: [nodeExternals()],  
   module: {
     rules: [
       {
@@ -22,6 +25,17 @@ module.exports = {
       }
     ]
   },
-  mode: 'development'
+  mode: 'production',
+  resolve: {
+    fallback: {
+      "async_hooks": false  
+    }
+  }
+  ignoreWarnings: [
+    {
+      module: /express/,  // Adjust this to match the specific module
+      message: /Critical dependency: the request of a dependency is an expression/
+    }
+  ]
 };
 
